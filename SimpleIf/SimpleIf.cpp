2a6eae1caf6fa1efd55f358d2cc45d6f9b593b0b
gseptarisa-ctrl <gseptarisa@gmail.com>
2026-03-13 21:57:15 +0700

    menambahkan program utama

--- SimpleIf/SimpleIf.cpp
+++ SimpleIf/SimpleIf.cpp
@@ -2,3 +2,16 @@
 #include <ctime>
 #include <cstdlib>
 using namespace std;
+
+int main(){
+    int x;
+
+    srand(time(0));
+    x = rand() % 10;
+
+    if (x == 5){
+        x = x + 1;
+        cout << "nilai x sudah dikoreksi menjadi : " << x << endl;
+    }
+    cout << "Bilangan yang diberikan = " << x;
+}
